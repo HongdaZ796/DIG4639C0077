2db7ac94952e760ebcb54e7a0d1a00fed147bc36
HongdaZ796 <homizheng00@gmail.com>
2019-09-17 17:31:51 -0400

    Version 1.0.1

--- Lab1/Entry.js
+++ Lab1/Entry.js
@@ -4,7 +4,11 @@ class Entry {
   }
 
   render() {
-    return `Hello, ${this.props.word}`;
+    return `<div>
+      <h1>${this.props.title}</h1>
+      <h4>${this.props.author} - ${this.props.releaseD}</h4>
+      <p>${this.props.description}</p>
+      </div>`;
   }
 
 
--- Lab1/index.js
+++ Lab1/index.js
@@ -3,7 +3,10 @@ const chalk = require('Chalk');
 
 let entry = new Entry(
   {
-    word: "world"
+    title: "Less (Winner of the Pulitzer Prize): A Novel",
+    Author: "Abdrew Sean Greer",
+    releaseD: "July 18, 2017",
+    description: "Image result for Less (Winner of the Pulitzer Prize): A Novel description 'Less' is a gay book by a gay author that won the Pulitzer Prize for fiction in 2018. This, in the same year that a gay journalist, Ronan Farrow (age 30), won a Pulitzer for his work."
   }
 );
 
