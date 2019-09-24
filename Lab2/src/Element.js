d8d985d9b6786ae8e816fc32ec0fdaa6bee790b4
HongdaZ796 <homizheng00@gmail.com>
2019-09-24 11:25:34 -0400

    ver 1.0.0

---
+++ Lab2/src/Element.js
@@ -0,0 +1,14 @@
+class Element {
+  constructor(props) {
+    this.element = document.createElement('props');
+    element.innerHTML = ""
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+module.exports = Element;
